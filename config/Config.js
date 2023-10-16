75504ed00f9954132a8aac64d2949569f7f73149
Inuyasha103 <zhangzhi103@gmail.com>
2023-10-17 00:16:23 +0800

    Update Config.js

--- config/Config.js
+++ config/Config.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
     BASE_URL : 'https://v8.9rnri.com/2048/',
-    ACCOUNT : '',
-    PASSWORD : '',
-    QUESTION : '',
-    ANSWER : '',
+    ACCOUNT : 'inuyasha233',
+    PASSWORD : 'tX5aE3dI63Eo',
+    QUESTION : '1',
+    ANSWER : '110',
 });
